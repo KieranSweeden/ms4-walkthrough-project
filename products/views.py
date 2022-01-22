32c756aa2a9d7037a3662335feefc31aac8e9b2d
Kieran Sweeden <kieransweeden@gmail.com>
2022-01-22 10:33:44 +0000

    Add search products query functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+
+# Q object used to generate search query
+# Using search filtering ob objects generically, only allows
+# && logic. So it'll filter down to those object that match the
+# query with it's product name AND description
+# The Q object allows us to do name OR description
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -9,10 +17,33 @@ def all_products(request):
 
     # Return all products within database using all()
     products = Product.objects.all()
+    query = None
 
+    if request.GET:
+        if 'q' in request.GET:
+
+            query = request.GET["q"]
+
+            # If query is blank ""
+            if not query:
+
+                # Inform user & redirect them back to the products url
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse("products"))
+
+            # | makes this an OR statement
+            # i in front of contains make this query case insensitive
+            # This is the query to be inserted into the filter object
+            # THink of this as the framework the filter filters by
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+            # Filter all products that match the query inserted
+            products = products.filter(queries)
+            
     # Add products to context to send them to template
     context = {
-        "products": products
+        "products": products,
+        "search_term": query,
     }
 
     return render(request, "products/products.html", context)
