2ea94cb8a633ac644ad0a823ff87ad39cfea4824
Kieran Sweeden <kieransweeden@gmail.com>
2022-02-05 11:18:51 +0000

    Include add product view and template

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 from . import views
 
+# What's important to note here is that we've specified
+# That the product_detail url requires an integer
+# If this were left as a regular variable, it would interpret
+# add/ as the value for the product_id variable
+# So if we used the url to navigate to the add_product page,
+# We'd actually get the product_detail page with an error,
+# As django looks for the first url it can match, it matches
+# with product_detail and thinkg add/ is a variable.
+
+# Using the int: helps tell django to skip
+
 urlpatterns = [
     path('', views.all_products, name="products"),
-    path('<product_id>', views.product_detail, name="product_detail")
+    path('<int:product_id>/', views.product_detail, name="product_detail"),
+    path('add/', views.add_product, name="add_product"),
 ]
