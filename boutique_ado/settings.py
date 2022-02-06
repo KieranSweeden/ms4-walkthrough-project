1732ebc65eafd4367372a67023af410c233389b6
Kieran Sweeden <kieransweeden@gmail.com>
2022-02-06 20:08:20 +0000

    Attempt fix for deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -278,7 +278,6 @@ DEFAULT_FROM_EMAIL = "boutiqueado@example.com"
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
